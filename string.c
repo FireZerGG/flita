7b81117129d1f734bb5f3c0e3960a7f35ef503b8
FireZerGG <91701962+FireZerGG@users.noreply.github.com>
2021-10-08 18:45:40 +0300

    Update string.c

--- string.c
+++ string.c
@@ -5,8 +5,7 @@
 int main(void)
 {
     char s[100000] = {0}; // double max length == 309
-    scanf("%100000s", s); // s - first element address, s[0] - first element, s[1] - second element
-    if(scanf("%100000s", s) != 1){
+    if(scanf("%100000s", s) != 1){  // s - first element address, s[0] - first element, s[1] - second element
         return 1;
     }
     int i = 0;
